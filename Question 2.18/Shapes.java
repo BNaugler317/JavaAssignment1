5cc3389e2c445ed63aac40e676e997c46fa177f4
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2025-07-17 14:54:37 -0300

    Added Question 2.18

---
+++ Question 2.18/Shapes.java
@@ -0,0 +1,17 @@
+// Question 2.18 Making shappes with asterix
+
+public class Shapes {
+  // main method begins execution of Java application
+
+  public static void main(String [] args) {
+    System.out.println("*********     ***       *        *");
+    System.out.println("*       *    *   *     ***      * *");
+    System.out.println("*       *   *     *   *****    *   *");
+    System.out.println("*       *   *     *     *     *     *");
+    System.out.println("*       *   *     *     *    *       *");
+    System.out.println("*       *   *     *     *     *     *");
+    System.out.println("*       *   *     *     *      *   *");
+    System.out.println("*       *    *   *      *       * *");
+    System.out.println("*********     ***       *        *");
+  } // end method main
+} // end class Numbers
