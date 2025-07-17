4c9f290551887fa046c01e0c880c01f9576c3493
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2025-07-17 11:50:27 -0300

    Added Question 2.14

---
+++ Question 2.14/Numbers.java
@@ -0,0 +1,9 @@
+// Question 2.14 make an application displaying numbers 1-4 on same line
+
+public class Numbers {
+  // main method begins execution of Java application
+
+  public static void main(String [] args) {
+    System.out.println("1 2 3 4");
+  } // end method main
+} // end class Numbers
