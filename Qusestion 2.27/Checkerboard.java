56ede1012b2880aa58b80ebfd69022342048ab7c
BNaugler317 <Brandon.Naugler@easternstudent.ca>
2025-07-17 17:12:39 -0300

    Added Question 2.27

---
+++ Qusestion 2.27/Checkerboard.java
@@ -0,0 +1,16 @@
+// Question 2.27 Make Checkerboard with asterix
+
+public class Checkerboard {
+  // main method begins execution of Java application
+
+  public static void main(String [] args) {
+    System.out.println("* * * * * * * * ");
+    System.out.println(" * * * * * * * * ");
+    System.out.println("* * * * * * * * ");
+    System.out.println(" * * * * * * * * ");
+    System.out.println("* * * * * * * * ");
+    System.out.println(" * * * * * * * * ");
+    System.out.println("* * * * * * * * ");
+    System.out.println(" * * * * * * * * ");
+  } // end method main
+} // end class Numbers
